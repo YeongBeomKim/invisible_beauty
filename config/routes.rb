b23c0ae517b40a57df6e5539b1d50d6c7962529e
sekwangoh <osktr5@gmail.com>
2017-11-12 07:38:05 +0900

    맞춤형 동영상 화면구성

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   root "home#index"
-  resources :posts, only: [:create,:show] do
+  resources :posts, only: [:create, :show, :index] do
     resources :metoos, only: [:create]
     post "echo", to: "echos#post_echo_toggle"
     post "comment_echo", to: "echos#comment_echo_toggle"
+    
   end
   resources :missions, only: [:create]
   resources :echos, only: [:create]
